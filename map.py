964693e71a73b221b3d33fb9455745263e208595
dk-hayashid <daisaku.hayashi@daikin.co.jp>
2022-03-01 10:57:38 +0900

    :rocket: support layout and multiple recommend location

--- map.py
+++ map.py
@@ -1,23 +1,28 @@
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
+from PIL import Image
 plt.rcParams["font.size"] = 18
 
 
-def return_close_value(array, target):
-    mindiff = np.inf
-    minvalue = None
-    for value in array:
-        diff = abs(value - target)
-        if diff < mindiff:
-            mindiff = diff
-            minvalue = value
-    return minvalue
+def return_close_value_idxes(measured_data, target, n=5):
+    diffs = [abs(measured_value - target) for measured_value in measured_data]
+    diffs_bottom_idxes = np.argsort(diffs)[:n]
+    return diffs_bottom_idxes
 
+def generate_cmap(colors):
+    """自分で定義したカラーマップを返す"""
+    values = range(len(colors))
+    vmax = np.ceil(np.max(values))
+    color_list = []
+    for v, c in zip(values, colors):
+        color_list.append( ( v/ vmax, c) )
+    return LinearSegmentedColormap.from_list('custom_cmap', color_list)
 
 def save_temperature_map(data: np.ndarray, target: float):
 
-    close_value = return_close_value(data, target)
+    close_value_idxes = return_close_value_idxes(data, target)
 
     # 図の生成
     fig = plt.figure(figsize=(5, 5))
@@ -29,7 +34,10 @@ def save_temperature_map(data: np.ndarray, target: float):
     ax.set_yticks(np.arange(0, 11, 1))
 
     # 温度マッピング
-    plt.pcolormesh(data.reshape(11, 5)[::-1,:], cmap="Reds", edgecolors='k', linewidth=2)
+    im = Image.open("room_layout.png")
+    cm = generate_cmap(['#B5D2EC','#FFFFFF','#FC5B4A'])
+    plt.pcolormesh(data.reshape(11, 5)[::-1,:], cmap=cm, edgecolors='k', linewidth=2, alpha=0.7)
+    ax.imshow(im, extent=[*ax.get_xlim(), *ax.get_ylim()], aspect='auto', alpha=1)
 
     # 誘導場所のピン打ち
     xmin, xmax = ax.get_xlim()
@@ -40,13 +48,10 @@ def save_temperature_map(data: np.ndarray, target: float):
     for y_coordinate in np.arange(ymin + height / 2, ymax, height):
         for x_coordinate in np.arange(xmin + width / 2, xmax, width):
             list_coordinate.append([x_coordinate, y_coordinate])
-    coordinate_target_list = []
-    for temperature, coordinate in zip(data, list_coordinate):
-        if temperature == close_value:
-            coordinate_target_list.append(coordinate)
+    list_coordinate_target = [list_coordinate[idx] for idx in close_value_idxes]
 
     image = plt.imread('./locater.png')
-    for coordinate_target in coordinate_target_list:
+    for coordinate_target in list_coordinate_target:
         ax.add_artist( 
             AnnotationBbox(
                 OffsetImage(image, zoom=0.06), (coordinate_target[0], coordinate_target[1]+height/2), frameon=False
